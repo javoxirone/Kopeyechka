4b49b22ed67c9d3993bd2430af5b5b0cab87c781
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-08-24 12:46:53 +0500

    Responsive slider

--- js/script.js
+++ js/script.js
@@ -8,5 +8,38 @@ $('.slider').slick({
     slidesToShow: 6,
     slidesToScroll: 2,
     arrows: true,
-    autoplay: false
+    autoplay: true,
+    dots: false,
+    responsive: [
+        {
+          breakpoint: 1001,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 481,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+            breakpoint: 321,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+        }
+      ]
 });
