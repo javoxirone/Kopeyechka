ea8717ca681f9471529baf06c0894142f596c1dc
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-08-24 19:05:27 +0500

    New page (goods-detail)

--- js/script.js
+++ js/script.js
@@ -53,3 +53,24 @@ $('.slider').slick({
         }
       ]
 });
+
+
+
+$('.detail-slider').slick({
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay: false,
+  dots: false,
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 480,
+      settings: {
+        fade: true,
+        cssEase: 'linear'
+      }
+    },
+  ]
+});
